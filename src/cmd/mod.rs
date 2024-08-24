e61b0b68d037b5561fe53fb1e8b6b6eb26c40d34
Yalong Zou <yalongz@hust.edu.cn>
2024-08-25 01:17:15 +0800

    revert: revert the last wrong patch.

--- src/cmd/mod.rs
+++ src/cmd/mod.rs
@@ -1,3 +1,2 @@
 pub mod create;
 pub mod v6pool;
-pub mod save_all;
--- src/config.rs
+++ src/config.rs
@@ -18,9 +18,6 @@ pub struct CommonConfig {
     #[serde(default = "default_libvirt_storage")]
     pub libvirt_storage: PathBuf,
 
-    #[serde(default = "default_libvirt_save")]
-    pub libvirt_save: PathBuf,
-
     #[serde(default = "default_libvirt_network")]
     pub libvirt_interface: String,
 }
@@ -36,12 +33,9 @@ pub struct Ipv6Config {
     pub wan_interface: String,
 }
 
+
 fn default_libvirt_storage() -> PathBuf {
     PathBuf::from("/var/lib/libvirt/images")
-}
-
-fn default_libvirt_save() -> PathBuf {
-    PathBuf::from("/var/lib/libvirt/qemu/save")
 }
 
 fn default_libvirt_network() -> String {
