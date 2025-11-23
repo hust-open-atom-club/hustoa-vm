2e4cc8f4427bd7080b219985f496fd0536d05b39
Yalong Zou <yalongz@hust.edu.cn>
2025-11-23 14:00:15 +0800

    fix: fix the osinfo required version.

--- src/config.rs
+++ src/config.rs
@@ -18,6 +18,9 @@ pub struct CommonConfig {
     #[serde(default = "default_libvirt_storage")]
     pub libvirt_storage: PathBuf,
 
+    #[serde(default = "default_libvirt_save")]
+    pub libvirt_save: PathBuf,
+
     #[serde(default = "default_libvirt_network")]
     pub libvirt_interface: String,
 }
@@ -33,9 +36,12 @@ pub struct Ipv6Config {
     pub wan_interface: String,
 }
 
-
 fn default_libvirt_storage() -> PathBuf {
     PathBuf::from("/var/lib/libvirt/images")
+}
+
+fn default_libvirt_save() -> PathBuf {
+    PathBuf::from("/var/lib/libvirt/qemu/save")
 }
 
 fn default_libvirt_network() -> String {
